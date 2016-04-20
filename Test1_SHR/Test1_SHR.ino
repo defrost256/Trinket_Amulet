40e4ec09af6e2bc64875033ae24aef030cc58c90
defrost256 <danko.barna@gmail.com>
2016-04-20 15:12:31 +0200

    I2C reading without interpretation
    Added RED LED error signals

--- Test1_SHR/Test1_SHR.ino
+++ Test1_SHR/Test1_SHR.ino
@@ -10,21 +10,18 @@
  * 
  */
 
- /*
-  * PWM Setup
-  * 1. Set PWM Mode ->  TCCR0A (0x2A) = 00000001
-  *                 ->  TCCR0B (0x33) = 00001(???) -> 3 bit CS for resolution (80. o)
-  * 2. Set Compare  ->  OCR0B  (0x28) = an 8 bit value defining the pulse width between 0 and 255
-  * 3. Set PWM Compare->TCCR0A (0x2A) = 00100001
-  */
+#include <Wire.h>
 
-int g = 1, clk = 3, ser_in = 4;
+char g = 1, clk = 3, ser_in = 4;
+char dom[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+char current[3];
 
 #define CTC 0     //No clear on OCR
 #define PWMclr 0  //PWMA disabled
 #define PWMset 1  //PWMA enabled
 #define COMA 2    //PWM Set to Clear on OCR and set on 0 (after OCRC)
-#define CS 12     //Prescaler set to CLK/1024 (with OCRC=255 it means a 32 Hz PWM) (with other OCRC values it's 8kHz/(OCRC + 1)
+#define CS 10     //Prescaler set to CLK/512 (with OCRC=255 it means a 64 Hz PWM) (with other OCRC values it's 8kHz/(OCRC + 1)
+#define CSoff 0   //Timer is off (no clock)
 #define PWMB 0    //PWMB disabled
 #define COMB 0    //OCRB disconnected
 #define FOC 0     //Force compare disabled
@@ -32,7 +29,15 @@ int g = 1, clk = 3, ser_in = 4;
 #define OCRA 51   //20% fill
 #define OCRC 255  //Full time PWM
 
-
+void errorSeq(int n){
+  char i;
+  for(i = 0; i < n; i++){
+    digitalWrite(g, 1);
+    delay(1000);
+    digitalWrite(g, 0);
+    delay(1000);
+  }
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -44,11 +49,24 @@ void setup() {
   GTCCR = (PWMB << 6) | (COMB << 4) | (FOC << 2) | (PSR << 1);
   OCR1A = OCRA;
   OCR1C = OCRC;
+  Wire.begin();
+  Wire.requestFrom(0x68, 7);
+  char i;
+  for(i = 0; i < 7; i++){
+    /*if(!Wire.available()){
+      errorSeq(2);
+      exit(0);
+    }*/
+    if(i > 3)
+      current[i - 4] = Wire.read();
+    else
+      Wire.read();
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int i;
+  char i;
   digitalWrite(ser_in, 1);
   for(i = 0; i < 8; i++){
     digitalWrite(g, 1);
